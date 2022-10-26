b3a2152b2efd0116bd7b1bd77ab1e63f0cda257b
Shawn19702 <shawnboodhoo5@gmail.com>
2022-10-26 09:35:32 -0400

    Spring Api

---
+++ src/test/java/com/shawn/jokesAPI/JokesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shawn.jokesAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
